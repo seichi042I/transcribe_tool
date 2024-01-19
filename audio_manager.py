a759cce3f3582e1c3372f2ef4efb04564fc37b08
seichi042I <seichi8901@gmail.com>
2024-01-19 16:27:25 +0900

    いらない部分を削除

--- audio_manager.py
+++ audio_manager.py
@@ -2,86 +2,35 @@ from typing import Union,Dict,Callable,List
 import os
 import threading
 
-import tkinter as tk
 from pydub import AudioSegment
 from pydub.playback import play
 from pathlib import Path
+import random
 
-
+OWNPATH = Path(__file__)
 
 class AudioManager:
     def __init__(self,wav_dirpath:Path,opts):
         self.wav_dirpath = wav_dirpath
         self.wav_path_list = [(wav_dirpath / f) for f in os.listdir(wav_dirpath) if f.endswith('.wav')]
-        # self.listbox:tk.Listbox = tk.Listbox(parent,**opts)
         self.current_index:int = 0
-        
-        # for file in wav_list:
-        #     self.listbox.insert(tk.END, file.stem)
-    
-    def next(self):
-        self.current_index += 1
-        return
-    
-    def back(self):
-        self.current_index -= 1
-        if self.current_index < 0:
-            self.current_index = 0
-        return
-    
-    def set(self,i:int):
-        self.current_index = i
-        return
+        self.play_error_sound_list = os.listdir(OWNPATH.parent / "system_voice" / "play_error")
         
     # ファイルを再生（スレッドで実行）
     def play(self,stem):
         name = stem+".wav"
         print((self.wav_dirpath / name))
         def run():
-            self.current_audio = AudioSegment.from_file((self.wav_dirpath / name))
-            play(self.current_audio)
+            try:
+                self.current_audio = AudioSegment.from_file((self.wav_dirpath / name))
+                play(self.current_audio)
+            except:
+                dir = OWNPATH.parent / "system_voice" / "play_error"
+                sound = self.play_error_sound_list[random.randint(0,4)]
+                _audio = AudioSegment.from_file(dir/sound)
+                play(_audio)
 
         thread = threading.Thread(target=run)
         thread.start()
-    
-    def on_select_item(self):
-        # self.current_index = self.listbox.curselection()[0]
-        # self.play()
-        return 'break'
-        
-        
-    def play_previous(self,args:Dict = {}):
-        # if self.wav_list:  # リストが空でないことを確認
-        #     if args['current_cursor_position']:
-        #         self.current_index = args['current_cursor_position']
-        #     self.back()
-            
-        #     self.listbox.selection_clear(0, tk.END)
-        #     self.listbox.selection_set(self.current_index)
-            
-        #     self.play()
-        self.play(args['stem'])
-        return "break"
-
-    def play_current(self,args:Dict = {}):
-        # if self.wav_list:
-        #     if args['current_cursor_position']:
-        #         self.current_index = args['current_cursor_position']
-        #     self.play()
-        self.play(args['stem'])
-        
-        return "break"
-
-    def play_next(self,args:Dict = {}):
-        # if self.wav_list:  # リストが空でないことを確認
-        #     if args['current_cursor_position']:
-        #         self.current_index = args['current_cursor_position']
-        #     self.next()
-            
-        #     self.listbox.selection_clear(0, tk.END)
-        #     self.listbox.selection_set(self.current_index)
-            
-        #     self.play()
-        self.play(args['stem'])
         
         return "break"
